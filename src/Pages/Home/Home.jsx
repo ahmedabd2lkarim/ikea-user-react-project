987bffbb1d2b5f689d9305b48fc8f079d49bc39e
ahmedabd2lkarim <ahmedabd2lkarim@gmail.com>
2025-05-17 15:53:15 +0300

    commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,51 +1,4 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-=======
-
-
-import React, { useState, useEffect } from "react";
-import Recommendedproducts from "../../Components/Product/ProductsCarousel/ProductsCarousel";
-import Loading from "../../Components/Loading/Loading";
-import "./Home.css";
-const Home = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch(`http://127.0.0.1:5000/api/products`);
-        const res = await response.json();
-        console.log(response);
-
-        setProducts(res.data);
-      } catch (err) {
-        setError(err.message);
-        console.error("Error fetching products:", err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-  console.log(products);
-
-  if (isLoading) return <Loading />;
-  if (error) return <div>Error: {error}</div>;
-
-  return (
-    <div className="home-container">
-      <section className="featured-products">
-        <h2 className="section-title">Featured Products</h2>
-        <Recommendedproducts products={products} />
-      </section>
-    </div>
-  );
-};
 import React from "react";
->>>>>>> origin/main
 import Teaser from "../../Components/Home/Teaser";
 import ProductScroller from "../../Components/Home/ProductScroller";
 import PromoScroller from "../../Components/Home/PromoScroller";
@@ -145,8 +98,7 @@ function Home() {
       {loadingHomeTeaser ? (
         <Box display="flex" justifyContent="center" my={5}>
           <CircularProgress />
-        </Box>
-      ) : firstTeaser ? (
+        </Box>      ) : firstTeaser ? (
         <Teaser
           title={firstTeaser?.title?.[language]}
           content={firstTeaser?.content?.[language]}
